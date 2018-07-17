bee133665dbd77b6995b3f83ca7880401619b643
stlefebvre <lefebvre.shelby@gmail.com>
2018-07-16 20:58:47 -0400

    added image, linked html to correct JS file

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ var app = express();
 var db = require("./models");
 var PORT = 3000;
 
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
